6d2ffda42976c04c94baab92b6bac25233e40402
hirohitohomma <35489914+hirohitohomma@users.noreply.github.com>
2021-01-19 23:55:04 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -5,26 +5,33 @@ function anserCheck() {
   var yourWeight = document.getElementById('ans_weight').value;
   var bmi = (yourWeight/(yourHeight/100)**2);
   console.log(bmi);
-   document.write(`こんにちは${yourName}さん<BR>${yourName}さんのBMIは<BR>${bmi.toFixed(2)}です<BR>`);
+
+
+
+   window.alert(`こんにちは${yourName}さん`)
+   window.alert(`${yourName}さんのBMIは${bmi.toFixed(2)}です`);
   var x = (bmi);
   console.log(x);
   if (x<16) {
-    document.write("痩せすぎです");
+    window.alert("痩せすぎです");
   } else if (x >= 16 && x < 17) {
-    document.write("痩せています");
+    window.alert("痩せています");
   } else if (x >= 17 && x <= 18.49) {
-    document.write("痩せ気味です");
+    window.alert("痩せ気味です");
   }else if (x >= 18.50 && x <= 24.99) {
-    document.write("普通体重です"); 
+    window.alert("普通体重です"); 
   }else if (x >= 25.00 && x <= 29.99) {
-    document.write("前肥満です"); 
+    window.alert("前肥満です"); 
   }else if (x >= 30.00 && x <= 34.99) {
-    document.write("肥満（1度）です"); 
+    window.alert("肥満（1度）です"); 
   }else if (x >= 35.00 && x <= 39.99) {
-    document.write("肥満（2度）です"); 
+    window.alert("肥満（2度）です"); 
   }else if (x >= 40.00) {
-    document.write("肥満（3度）です"); 
+    window.alert("肥満（3度）です"); 
   }
+
+
+
 }
 
 var head =document.head;
